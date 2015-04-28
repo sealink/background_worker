575191622ef9fb0fb6dd7561858e1e68fa54b64f
Adam Davies <adzdavies@gmail.com>
2015-04-28 16:39:25 +0930

    Provide override for how to enqueue

--- lib/background_worker.rb
+++ lib/background_worker.rb
@@ -11,4 +11,20 @@ module BackgroundWorker
       @@after_exception if defined?(@@after_exception)
     end
   end
+
+  # Provide your own background worker enqueue implementation
+  #   eg:
+  #  Resque.enqueue(klass, method_name, options)
+  def self.enqueue(klass, method_name, options)
+    if @@enqueue_with
+      @@enqueue_with.call(klass, method_name, options)
+    else
+      # No backgrounded by default
+      klass.perform(method_name, options)
+    end
+  end
+
+  def self.enqueue_with(&block)
+    @@enqueue_with = block
+  end
 end
